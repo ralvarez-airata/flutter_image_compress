0f6f56c47b8f0d4f7dedb4beda0dd7250d450ea3
Roberto Alvarez <92732665+ralvarez-airata@users.noreply.github.com>
2025-08-14 11:52:49 -0300

    Update Package.swift

--- packages/flutter_image_compress_common/ios/Package.swift
+++ packages/flutter_image_compress_common/ios/Package.swift
@@ -9,12 +9,13 @@ let package = Package(
         .library(name: "CompressImageCommon", targets: ["CompressImageCommon"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.0.0")
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.0.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImageWebPCoder", from: "0.14.6")
     ],
     targets: [
         .target(
             name: "CompressImageCommon",
-            dependencies: ["SDWebImage"]
+            dependencies: ["SDWebImage", "SDWebImageWebPCoder"]
         )
     ]
 )
